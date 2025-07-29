3f298406612d75d8cd2c58df845b7f59ecfb6e44
Juno BAEK <laegel1@gmail.com>
2025-07-28 20:55:36 -0700

    add webpack

---
+++ cryptoapp/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+        open: true
+    },
+    mode: 'development',
+
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ]
+}
