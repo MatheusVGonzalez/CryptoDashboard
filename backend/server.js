44daa61442c5ebee6463b617bec90ab064dfbcfd
MatheusVGonzalez <matheusverissimogonzalez@gmail.com>
2025-07-31 16:09:40 -0700

    Adjust some codes

--- backend/server.js
+++ backend/server.js
@@ -37,52 +37,45 @@
       }
   });
 
-  app.get("/user-balance/:nickName", (req, res) => {
-  const { nickName } = req.params;
-  const data = fs.readFileSync("./user.json");
-  const users = JSON.parse(data);
-  const user = users.find((u) => u.nickName === nickName);
-  if (user) {
-    res.json({ balance: user.balance || 0 });
-  } else {
-    res.status(404).json({ message: "User not found" });
-  }
-});
+    app.get("/user-balance/:nickName", (req, res) => {
+    const { nickName } = req.params;
+    const data = fs.readFileSync("./user.json");
+    const users = JSON.parse(data);
+    const user = users.find((u) => u.nickName === nickName);
+    if (user) {
+      res.json({ balance: user.balance || 0 });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  });
 
-app.post("/update-balance", (req, res) => {
-  const { nickName, balance } = req.body;
-
-  const data = fs.readFileSync("./user.json");
-  const users = JSON.parse(data);
-
-  const userIndex = users.findIndex((u) => u.nickName === nickName);
-  if (userIndex !== -1) {
-    users[userIndex].balance = balance;
-    fs.writeFileSync("./user.json", JSON.stringify(users, null, 2));
-    res.status(200).json({ message: "Balance updated" });
-  } else {
-    res.status(404).json({ message: "User not found" });
-  }
-});
+  app.post("/update-balance", (req, res) => {
+    const { nickName, balance } = req.body;
+    const data = fs.readFileSync("./user.json");
+    const users = JSON.parse(data);
+    const userIndex = users.findIndex((u) => u.nickName === nickName);
+    if (userIndex !== -1) {
+      users[userIndex].balance = balance;
+      fs.writeFileSync("./user.json", JSON.stringify(users, null, 2));
+      res.status(200).json({ message: "Balance updated" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  });
 
 
   app.post("/register", (req, res) => {
     const { nickName, password } = req.body;
-
     if (!nickName || !password) {
       return res.status(400).json({ message: "Missing fields" });
     }
-
     const data = fs.readFileSync("./user.json");
     const users = JSON.parse(data);
-
     const userExists = users.find((u) => u.nickName === nickName);
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
-
     users.push({ nickName, password });
-
     fs.writeFileSync("./user.json", JSON.stringify(users, null, 2));
     return res.status(201).json({ message: "User registered" });
   });
